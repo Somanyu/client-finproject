2a1567b4135e4826c07b60365f85923cffea56ae
Somanyu <somanyu.03samal@gmail.com>
2023-02-26 11:52:30 +0530

    🛠️ chore: Install and configure Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-green-600">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
