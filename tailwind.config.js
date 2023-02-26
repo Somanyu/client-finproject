73198ebedcf0053fc43a7b8c8d796ef5605eaeac
Somanyu <somanyu.03samal@gmail.com>
2023-02-26 12:08:49 +0530

    🛠️ feat: Configure Flowbite components with Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 };
