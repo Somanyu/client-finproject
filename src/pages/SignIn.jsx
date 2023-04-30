57a4dbce854d48a28449135eb8a5f74caf8552f9
Somanyu <somanyu.03samal@gmail.com>
2023-04-30 07:30:16 +0530

    💄 update: few UI changes

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -24,17 +24,22 @@ function SignIn() {
 
               <div className="relative z-0 mb-7">
                 <input type="text" id="floating_standard" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
-                <label htmlFor="floating_standard" className="absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full name</label>
-              </div>
-
-              <div className="relative z-0 mb-7">
-                <input type="text" id="floating_standard" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
                 <label htmlFor="floating_standard" className="absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
               </div>
 
               <div className="relative z-0 mb-7">
                 <input type="text" id="floating_standard" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
                 <label htmlFor="floating_standard" className="absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+              </div>
+
+              <div className="flex items-start pb-5">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input id="remember" type="checkbox" value="" className="font-roboto w-4 h-4 border border-gray-300 text-teal-500 rounded bg-gray-50 focus:ring-3 focus:ring-teal-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                  </div>
+                  <label htmlFor="remember" className="ml-2 text-sm font-medium font-roboto text-gray-900 dark:text-gray-300">Remember me</label>
+                </div>
+                <a href="/" className="ml-auto text-sm text-teal-700 hover:underline font-roboto dark:text-blue-500">Lost Password?</a>
               </div>
 
               <div className="flex flex-col mt-12">
