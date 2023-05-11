9842d66ee706eabc6842c3ae2e25f06baf19d545
Somanyu <somanyu.03samal@gmail.com>
2023-05-11 22:50:37 +0530

    feat: Implement form submission using Axios and React setState()

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,12 +1,35 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-inner-declarations */
 /* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  // eslint-disable-next-line no-unused-vars
+  const [data, setData] = useState(null);
+  // const [error, setError] = useState(null);
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSignUpSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:3001/auth/register', formData)
+      .then((res) => {
+        // console.log(res.data);
+        setData(res.data);
+      });
+  };
+
   return (
     <div>
       <div className="flex relative p-8">
@@ -18,27 +41,32 @@ function SignUp() {
 
           {/* First Content */}
           <div className="col-span-12 lg:col-span-4 md:col-span-4 py-12 px- md:px-5 lg:px-10 xl:px-28 text-sm rounded-lg bg-transparent dark:bg-gray-800 dark:text-blue-400" role="alert">
-            <form className="animate__animated animate__slideInLeft animate__faster">
+            <form onSubmit={handleSignUpSubmit} className="animate__animated animate__slideInLeft animate__faster">
               <h1 className="font-bold text-gray-900 dark:text-white text-left font-archivo text-3xl">Create an account</h1>
               <p className="text-left mb-14 mt-2 font-light text-gray-500 dark:text-gray-400 font-roboto">Let&#39;s get you started with expenses with a free account.</p>
 
               <div className="relative z-0 mb-7">
-                <input type="text" id="floating_standard" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
+                <input type="text" id="floating_standard1" name="fullName" onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
                 <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full name</label>
               </div>
 
               <div className="relative z-0 mb-7">
-                <input type="text" id="floating_standard" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
+                <input type="email" id="floating_standard2" name="email" onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
                 <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
               </div>
 
               <div className="relative z-0 mb-7">
-                <input type="text" id="floating_standard" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
+                <input type="tel" id="floating_standard3" name="phone" onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
+                <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone</label>
+              </div>
+
+              <div className="relative z-0 mb-7">
+                <input type="password" id="floating_standard4" name="password" onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
                 <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
               </div>
 
               <div className="flex flex-col mt-12">
-                <button type="button" className="text-white mb-3 hover:drop-shadow-lg bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300 font-medium rounded-full text-base px-5 py-2.5 text-center mr-2 dark:bg-blue-600 font-archivo">Create account</button>
+                <button type="submit" className="text-white mb-3 hover:drop-shadow-lg bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300 font-medium rounded-full text-base px-5 py-2.5 text-center mr-2 dark:bg-blue-600 font-archivo">Create account</button>
                 <button type="button" className="text-black flex justify-center bg-transparent border-2 border-black focus:outline-none focus:ring-4 focus:ring-gray-400 font-medium rounded-full text-base px-5 py-2.5 text-center mr-2 mb-2 font-archivo">
                   <FcGoogle className="text-2xl mr-2" />
                   Sign up with Google
