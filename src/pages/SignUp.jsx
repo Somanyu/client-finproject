5e8bd7a1d1ff4c49e3a0e12ad7384b56e0262096
Somanyu <somanyu.03samal@gmail.com>
2023-05-17 21:46:50 +0530

    feat: Validated user inputs with Formik and Yup and display errors to user.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,33 +3,34 @@
 /* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+const SignUpSchema = Yup.object().shape({
+  fullName: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Field cannot be empty')
+    .matches(/^[a-zA-Z ]+$/, 'Only alphabets are allowed'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Field cannot be empty'),
+  phone: Yup.string()
+    .min(10, 'Too Short!')
+    .max(14, 'Too Long!')
+    .required('Field cannot be empty')
+    .matches(/^[0-9]+$/, 'Only numbers are allowed'),
+  password: Yup.string()
+    .min(8, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Field cannot be empty'),
+});
 
 function SignUp() {
-  const [formData, setFormData] = useState({});
-  // eslint-disable-next-line no-unused-vars
-  const [data, setData] = useState(null);
-  // const [error, setError] = useState(null);
-
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleSignUpSubmit = (event) => {
-    event.preventDefault();
-    axios.post('http://localhost:3001/auth/register', formData)
-      .then((res) => {
-        // console.log(res.data);
-        setData(res.data);
-      });
-  };
-
   return (
     <div>
       <div className="flex relative p-8">
@@ -41,38 +42,71 @@ function SignUp() {
 
           {/* First Content */}
           <div className="col-span-12 lg:col-span-4 md:col-span-4 py-12 px- md:px-5 lg:px-10 xl:px-28 text-sm rounded-lg bg-transparent dark:bg-gray-800 dark:text-blue-400" role="alert">
-            <form onSubmit={handleSignUpSubmit} className="animate__animated animate__slideInLeft animate__faster">
-              <h1 className="font-bold text-gray-900 dark:text-white text-left font-archivo text-3xl">Create an account</h1>
-              <p className="text-left mb-14 mt-2 font-light text-gray-500 dark:text-gray-400 font-roboto">Let&#39;s get you started with expenses with a free account.</p>
-
-              <div className="relative z-0 mb-7">
-                <input type="text" id="floating_standard1" name="fullName" onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
-                <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full name</label>
-              </div>
-
-              <div className="relative z-0 mb-7">
-                <input type="email" id="floating_standard2" name="email" onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
-                <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
-              </div>
-
-              <div className="relative z-0 mb-7">
-                <input type="tel" id="floating_standard3" name="phone" onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
-                <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone</label>
-              </div>
-
-              <div className="relative z-0 mb-7">
-                <input type="password" id="floating_standard4" name="password" onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
-                <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-              </div>
-
-              <div className="flex flex-col mt-12">
-                <button type="submit" className="text-white mb-3 hover:drop-shadow-lg bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300 font-medium rounded-full text-base px-5 py-2.5 text-center mr-2 dark:bg-blue-600 font-archivo">Create account</button>
-                <button type="button" className="text-black flex justify-center bg-transparent border-2 border-black focus:outline-none focus:ring-4 focus:ring-gray-400 font-medium rounded-full text-base px-5 py-2.5 text-center mr-2 mb-2 font-archivo">
-                  <FcGoogle className="text-2xl mr-2" />
-                  Sign up with Google
-                </button>
-              </div>
-            </form>
+            <Formik
+              initialValues={{
+                fullName: '', email: '', phone: '', password: '',
+              }}
+              validationSchema={SignUpSchema}
+              onSubmit={async (values, { resetForm }) => {
+                try {
+                  await axios.post('http://localhost:3001/auth/register', values);
+                  resetForm({ values: '' });
+                  // console.log(res.data);
+                } catch {
+                  // console.error('error');
+                }
+              }}
+            >
+              {({ errors, touched }) => (
+                <Form>
+                  <h1 className="font-bold text-gray-900 dark:text-white text-left font-archivo text-3xl">Create an account</h1>
+                  <p className="text-left mb-14 mt-2 font-light text-gray-500 dark:text-gray-400 font-roboto">Let&#39;s get you started with expenses with a free account.</p>
+                  <div className="relative z-0 mb-7">
+                    <Field type="text" id="floating_standard1" name="fullName" className="block py-2.5 px-0 w-full text-sm peer text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
+                    {errors.fullName && touched.fullName ? (
+                      <p className="peer-invalid:visible font-roboto text-red-600 text-sm">
+                        {errors.fullName}
+                      </p>
+                    ) : null}
+                    <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full name</label>
+                  </div>
+                  <div className="relative z-0 mb-7">
+                    <Field type="email" id="floating_standard2" name="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
+                    {errors.email && touched.email ? (
+                      <p className="peer-invalid:visible font-roboto text-red-600 text-sm">
+                        {errors.email}
+                      </p>
+                    ) : null}
+                    <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+                  </div>
+                  <div className="relative z-0 mb-7">
+                    <Field type="tel" id="floating_standard3" name="phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
+                    {errors.phone && touched.phone ? (
+                      <p className="peer-invalid:visible font-roboto text-red-600 text-sm">
+                        {errors.phone}
+                      </p>
+                    ) : null}
+                    <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone</label>
+                  </div>
+                  <div className="relative z-0 mb-7">
+                    <Field type="password" id="floating_standard4" name="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-emerald-600 peer" placeholder=" " />
+                    {errors.password && touched.password ? (
+                      <p className="peer-invalid:visible font-roboto text-red-600 text-sm">
+                        {errors.password}
+                      </p>
+                    ) : null}
+                    <label htmlFor="floating_standard" className="after:content-['*'] after:ml-0.5 after:text-red-500 absolute font-roboto text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-emerald-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                  </div>
+                  <div className="flex flex-col mt-12">
+                    <button type="submit" className="text-white mb-3 hover:drop-shadow-lg bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300 font-medium rounded-full text-base px-5 py-2.5 text-center mr-2 dark:bg-blue-600 font-archivo">Create account</button>
+                    <button type="button" className="text-black flex justify-center bg-transparent border-2 border-black focus:outline-none focus:ring-4 focus:ring-gray-400 font-medium rounded-full text-base px-5 py-2.5 text-center mr-2 mb-2 font-archivo">
+                      <FcGoogle className="text-2xl mr-2" />
+                      Sign up with Google
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
             <p className="mb-3 mt-4 md:block lg:hidden xl:hidden font-light text-gray-500 dark:text-gray-400 font-roboto text-center">
               Already have a account.
               <Link to="/signin" className="font-medium text-blue-600 underline dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:no-underline pl-1">Log in</Link>
