c77a678cb000dda19d7589dcb291e7c5e983968d
Somanyu <somanyu.03samal@gmail.com>
2023-05-23 09:44:36 +0530

    feat: Protect routes bases on isLoggedIn state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Routes, Route, Navigate,
+} from 'react-router-dom';
 import 'animate.css';
+import { useSelector } from 'react-redux';
 
 import './App.css';
 import SignIn from './pages/SignIn';
@@ -9,12 +12,20 @@ import SignUp from './pages/SignUp';
 import User from './pages/User';
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/" element={<SignUp />} />
-        <Route path="/user" element={<User />} />
+        {/* {isLoggedIn ? (
+          <Route path="/user" element={<User />} />
+        ) : (
+          <Route path="/user" element={<Navigate replace to="/signin" />} />
+        )} */}
+
+        {isLoggedIn && <Route path="/user" element={<User />} />}
+        <Route path="/user" element={<Navigate replace to="/signin" />} />
       </Routes>
     </BrowserRouter>
   );
