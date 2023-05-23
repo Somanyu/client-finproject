e57b583db9a378ed8711f3350d05a5af3caf7c03
Somanyu <somanyu.03samal@gmail.com>
2023-05-23 09:41:52 +0530

    add: Add redux to store logged in state to use across components.

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-param-reassign */
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: { isLoggedIn: false },
+  reducers: {
+    setLoggedIn(state) {
+      state.isLoggedIn = true;
+    },
+    setLoggedOut(state) {
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+
+export const store = configureStore({
+  reducer: authSlice.reducer,
+});
