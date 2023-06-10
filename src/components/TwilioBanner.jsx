00cfd538fdda3b39ff90ec079b9d5b265928077c
Somanyu <somanyu.03samal@gmail.com>
2023-06-10 23:27:39 +0530

    fix: Create background shadow when modal's open.

--- src/components/TwilioBanner.jsx
+++ src/components/TwilioBanner.jsx
@@ -36,7 +36,7 @@ function TwilioBanner() {
         data-modal-backdrop="static"
         tabIndex="-1"
         aria-hidden="true"
-        className={`fixed flex justify-center items-center top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full ${visible ? '' : 'hidden'}`}
+        className={`fixed flex justify-center items-center bg-slate-300/50 top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full ${visible ? '' : 'hidden'}`}
       >
         <div className="relative w-full max-w-2xl max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
