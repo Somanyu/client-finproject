6db5c74a560fa76064f1187dae0c70391ba17eda
Somanyu <somanyu.03samal@gmail.com>
2023-06-14 07:45:20 +0530

    fix: Error in namespace and protocol.

--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -4,14 +4,14 @@
 
 function apiEndpoints() {
   return {
-    register: '/auth/register',
-    login: '/auth/login',
-    logout: '/auth/logout',
-    twilioVerify: '/verify',
-    refresh: '/refresh',
-    user: '/user',
-    updateUser: '/user/update',
-    deleteUser: '/user/delete',
+    register: 'auth/register',
+    login: 'auth/login',
+    logout: 'auth/logout',
+    twilioVerify: 'verify',
+    refresh: 'refresh',
+    user: 'user',
+    updateUser: 'user/update',
+    deleteUser: 'user/delete',
   };
 }
 
